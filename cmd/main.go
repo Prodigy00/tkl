a7ec68e9176a16c6b0c383b24dc23e850e4c2701
Gideon Idowu <simplygiddy@gmail.com>
2023-06-07 14:44:45 +0100

    remove redundant comment in main

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,6 @@ func main() {
 	}
 
 	cmd, args := args[0], args[1:]
-	//fmt.Printf(" cmd: %s || args: %s ", cmd, args)
 
 	switch cmd {
 	case "add":
@@ -34,36 +33,3 @@ func main() {
 		log.Fatalf("Unrecognized command %q. Command must be one of add, list, update, delete", cmd)
 	}
 }
-
-//replace task file struct with a file that will be created if it doesn't already exist
-
-//func root(args []string) error {
-//	if len(args) < 1 {
-//		return errors.New("You must pass a sub-command")
-//	}
-//
-//	cmds := []tasklist.Runner{
-//		tasklist.NewAddTaskCommand(),
-//	}
-//
-//	subcommand := os.Args[1]
-//
-//	for _, cmd := range cmds {
-//		if cmd.Name() == subcommand {
-//			err := cmd.Init(os.Args[2:])
-//			if err != nil {
-//				log.Fatal(err)
-//			}
-//			return cmd.Run()
-//		}
-//	}
-//
-//	return fmt.Errorf("Unknown subcommand: %s", subcommand)
-//}
-//
-//func main() {
-//	if err := root(os.Args[1:]); err != nil {
-//		fmt.Println(err)
-//		os.Exit(1)
-//	}
-//}
